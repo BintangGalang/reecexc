443a8b5b3886e7d109800ef62bc0e643d8070001
BintangGalang <163822567+BintangGalang@users.noreply.github.com>
2024-06-29 10:30:48 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,30 @@
-// nuxt.config.ts
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // Your Nuxt config here
-  typescript: {
-    strict: true
-  }
-})
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  routeRules: {
+    // prerender index route by default
+    '/': { prerender: true },
+  },
+
+  modules: ["@nuxt/ui", "nuxt-vuefire"],
+  vuefire: {
+    config: {
+      apiKey: "AIzaSyAbPtOxcC3KRjSgk6bpqWhcJ0hkqHOIIIQ",
+      authDomain: "project-ef619.firebaseapp.com",
+      projectId: "project-ef619",
+      storageBucket: "project-ef619.appspot.com",
+      messagingSenderId: "1088765065398",
+      appId: "1:1088765065398:web:4a21d06589f9610512ce17",
+      measurementId: "G-B0J9L7BGVP"
+    },
+  },
+});
